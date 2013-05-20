557abcaf8befc4d6a76ae2c7f7ac759d71b0d82e
Alex Propes <alex.propes@gmail.com>
2013-05-20 12:18:27 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MysidePrelaunch::Application.config.secret_token = 'c5584126f5f98dee1f1e6a5ac0c5631d28f66f140830b64aabd37c8c334f1dcbba7f5c5be3bbba5f4689814e6803f69ebf053f69978adbdce08be61da73a7542'
